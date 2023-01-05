caa67c9e00f37ef55030534dce22b73cbe5af67d
RobertDoms <robertdoms90@gmail.com>
2023-01-05 09:40:43 +0200

    Update ImageGalleryItem.jsx

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import { StyledLi, StyledItemImage } from './ImageGalleryItem.styled';
+import Modal from '../Modal';
+import PropTypes from 'prop-types';
+
+class GalleyItem extends Component {
+  state = {
+    showModal: false,
+  };
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+  render() {
+    const { tags, webformatURL, largeImageURL } = this.props;
+    const { showModal } = this.state;
+    return (
+      <StyledLi>
+        <StyledItemImage
+          onClick={this.toggleModal}
+          src={webformatURL}
+          alt={tags}
+        />
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src={largeImageURL} alt={tags} />
+          </Modal>
+        )}
+      </StyledLi>
+    );
+  }
+}
+GalleyItem.propTypes = {
+  tags: PropTypes.string,
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+};
+
+export default GalleyItem;
