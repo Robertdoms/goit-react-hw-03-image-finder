a01d4f9a5478e284e5b62b30a042c96132cf6437
RobertDoms <robertdoms90@gmail.com>
2023-01-10 00:51:54 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -25,7 +25,7 @@ class Searchbar extends Component {
       return;
     }
     this.props.onSubmit(this.state.searchQuery);
-    
+    this.setState({ searchQuery: '' });
   };
   render() {
     return (
