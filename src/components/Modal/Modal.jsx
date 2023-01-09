3925956fc5b661d68011fde4074acc53448adc00
RobertDoms <robertdoms90@gmail.com>
2023-01-10 00:43:20 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,6 +3,8 @@ import { createPortal } from 'react-dom';
 import { ModalOverlay, ModalWindow } from './Modal.styled';
 import PropTypes from 'prop-types';
 
+const modalRoot = document.querySelector('#modal-root');
+
 export default class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
@@ -25,10 +27,12 @@ export default class Modal extends Component {
     return createPortal(
       <ModalOverlay onClick={this.handleBackDropClick}>
         <ModalWindow>{this.props.children}</ModalWindow>
-      </ModalOverlay>
+      </ModalOverlay>,
+      modalRoot
     );
   }
 }
 Modal.propTypes = {
+  ModalWindow: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
 };
