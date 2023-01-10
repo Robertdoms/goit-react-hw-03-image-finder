59d7de8ee808533838d568e1ce4defd9cbce1a91
RobertDoms <robertdoms90@gmail.com>
2023-01-11 00:08:29 +0200

    Update Searchbar.styled.jsx

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -82,40 +82,4 @@ export const StyledInput = styled.input`
     font-size: 16px;
   }
 `;
-/*
- * Стили компонента SearchForm
- */
-// .SearchForm {
 
-// }
-
-// .SearchForm-button {
-//   display: inline-block;
-//   width: 48px;
-//   height: 48px;
-//   border: 0;
-//   background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
-//   background-size: 40%;
-//   background-repeat: no-repeat;
-//   background-position: center;
-//   opacity: 0.6;
-//   transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
-//   cursor: pointer;
-//   outline: none;
-// }
-
-// .SearchForm-button:hover {
-
-// }
-
-// .SearchForm-button-label {
-
-// }
-
-// .SearchForm-input {
-
-// }
-
-// .SearchForm-input::placeholder {
-
-// }
